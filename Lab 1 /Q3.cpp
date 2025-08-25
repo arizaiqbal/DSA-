9458318c4eebd548b3711cf1757b9eb778c4be4f
Ariza Iqbal <areezashaikh23@gmail.com>
2025-08-25 21:52:59 +0500

    Update Q3.cpp

--- Lab 1 /Q3.cpp
+++ Lab 1 /Q3.cpp
@@ -46,22 +46,22 @@ class Box{
 int main(){
     cout << "Deep Copy" << endl;
     Box b1(10);
-    Box b2 = b1;
-    b2.setSize(20);
+    Box b2 = b1; //new memory is allocated and value of 10 is duplicated to this new space, after copying b1 into b2, changing b2's size does not modify b1, because it has its own memory
+    b2.setSize(20);  //only changes b2 value
 
     cout << "Box 1 size " << b1.getSize() << endl;
     cout << "Box 2 size " << b2.getSize() << endl;
 
     Box b3;
-    b3 = b1;
-    b3.setSize(30);
+    b3 = b1; //copy assignment operator
+    b3.setSize(30);  //it will only change b3 memory
 
     cout << "Box 1 size " << b1.getSize() << endl;
     cout << "Box 3 size " << b3.getSize() << endl;
 
     cout << "Shallow Copy" << endl;
     int* s1 = new int(50);
-    int* s2 = s1;  
+    int* s2 = s1;    //shallow copy as both will point to SAME memory, creating a dangling pointer and double deletion
     cout << "s1 = " << *s1 << endl;
     cout << "s2 = " << *s2 << endl;
 
